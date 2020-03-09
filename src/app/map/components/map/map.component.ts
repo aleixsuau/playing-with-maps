56134eb7317940cf5c2e0b0eb714e89548793259
Aleix Suau <info@macrofonoestudio.es>
2020-03-09 13:56:23 +0100

    provisional changes

--- src/app/map/components/map/map.component.ts
+++ src/app/map/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnChanges, ViewChild, ElementRef, Input, SimpleChanges } from '@angular/core';
+import MarkerClusterer from '@google/markerclustererplus';
 
 
 @Component({
@@ -29,19 +30,30 @@ export class MapComponent implements OnChanges {
       const coordinates = new google.maps.LatLng(this.lat, this.lng);
       const mapOptions = {
         center: coordinates,
-        zoom: 8,
+        zoom: 5,
       };
       this.map = new google.maps.Map(this.gmap.nativeElement, mapOptions);
 
-      days.forEach((experiences, i) => {
-        experiences.forEach(experience => {
+      // Create markers
+      const daysMarkers = days.map((experiences, i) => {
+        return experiences.map(experience => {
           return new google.maps.Marker({
             position: new google.maps.LatLng(Number(experience.lat), Number(experience.lng)),
             label: experience.title,
-            map: this.map,
+            // map: this.map,
           });          
         });
       });
+      const allMarkers = daysMarkers.reduce((result, current) => [...result, ...current], []);
+      // Group markers in clusters and place them into the map
+      var markerCluster = new MarkerClusterer(this.map, allMarkers,
+                                  {
+                                    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+                                    title: 'hey map'
+                                  });
+
+      console.log('markerCluster', markerCluster)
+
     }
   }
 }
